b3dfdb0969775db9c23ce71fac03ddbfe9c1cfd5
Siddharth Bhal <siddharth0bhal@gmail.com>
2016-10-26 06:45:50 -0400

    rearranged directries

--- rest/routes/index.js
+++
@@ -1,4 +0,0 @@
-var devices = require('./devices.js');
-var other = require('./other.js');
-
-module.exports = [].concat(devices, other);
--- rest/routes/other.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-        {
-        method: 'POST',
-        path: '/device',
-        handler: { act: 'role:restRequest, cmd:registerDevice' } // will hit the registerDevice pattern using funky jsonic syntax
-    }
-];
